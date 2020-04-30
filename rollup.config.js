3472b9e29fb9b243e7e81bdc2056ce9f577c42b5
Yanzhen Yu <yanzhen@smartx.com>
2020-04-30 21:39:55 +0800

    close #205 1. upgrade rollup postcss plugin 2. fix postcss plugins for bundles

--- rollup.config.js
+++ rollup.config.js
@@ -48,16 +48,13 @@ const baseConfigs = [
 let configs = [];
 
 for (const c of baseConfigs) {
-  const plugins = [
-    resolve(),
-    commonjs({ namedExports }),
-    typescript(),
+  const basePlugins = [resolve(), commonjs({ namedExports }), typescript()];
+  const plugins = basePlugins.concat(
     postcss({
       extract: false,
       inject: false,
     }),
-  ];
-  const minifyPlugins = plugins.concat(terser());
+  );
   // browser
   configs.push({
     input: c.input,
@@ -73,7 +70,14 @@ for (const c of baseConfigs) {
   // browser + minify
   configs.push({
     input: c.input,
-    plugins: minifyPlugins,
+    plugins: basePlugins.concat(
+      postcss({
+        extract: true,
+        minimize: true,
+        sourceMap: true,
+      }),
+      terser(),
+    ),
     output: [
       {
         name: c.name,
