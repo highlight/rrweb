836b11daae4412a8e163f5464ad434fc17ec0831
John Pham <john@highlight.run>
2021-09-22 16:52:59 -0700

    Set the blockClass background color to the same as the color

--- src/replay/styles/inject-style.ts
+++ src/replay/styles/inject-style.ts
@@ -1,8 +1,7 @@
 const rules: (blockClass: string) => string[] = (blockClass: string) => [
-  `.${blockClass} { background: #ccc }`,
   'noscript { display: none !important; }',
-  `.${blockClass} { background: black; border-radius: 5px; }`,
-  `.${blockClass}:hover::after {content: 'Redacted'; color: white; text-align: center; width: 100%; display: block;}`,
+  `.${blockClass} { background: currentColor; border-radius: 5px; }`,
+  `.${blockClass}:hover::after {content: 'Redacted'; color: white; background: black; text-align: center; width: 100%; display: block;}`,
 ];
 
 export default rules;
