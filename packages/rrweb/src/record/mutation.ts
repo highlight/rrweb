862976e2da5287c2cbf540b0b6ac4de9242966d9
Vadim Korolik <vkorolik@gmail.com>
2022-08-18 16:26:56 -0700

    fix client mutation observer check
    ensure the element on which `.contains()` is called is a node.

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -279,7 +279,9 @@ export default class MutationBuffer {
       // ensure shadowHost is a Node, or doc.contains will throw an error
       const notInDoc =
         !this.doc.contains(n) &&
-        (!rootShadowHost || !this.doc.contains(rootShadowHost));
+        (!rootShadowHost ||
+          !(rootShadowHost instanceof Node) ||
+          !this.doc.contains(rootShadowHost));
       if (!n.parentNode || notInDoc) {
         return;
       }
