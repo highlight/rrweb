5cbd07f5782f9b60b0716388f52537960c678920
Vadim Korolik <vkorolik@gmail.com>
2022-09-29 10:29:03 -0700

    support for highlight yarn workspaces monorepo

--- packages/rrweb-player/typings/index.d.ts
+++ packages/rrweb-player/typings/index.d.ts
@@ -1,6 +1,6 @@
 import { playerConfig } from '@highlight-run/rrweb/typings/types';
 import { eventWithTime } from '@highlight-run/rrweb-types';
-import { Replayer, mirror } from '@highlight-run/rrweb';
+import { Replayer, mirror } from '@highlight-run/rrweb/typings';
 import { SvelteComponent } from 'svelte';
 
 export type RRwebPlayerOptions = {
