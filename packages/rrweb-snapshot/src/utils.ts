c273ac2b68cadc7514ad429a2c6bd09aa8074fb8
Spencer <spencer@highlight.io>
2023-10-16 12:45:13 -0400

    Don't use \d cause it gets reverted

--- packages/rrweb-snapshot/src/utils.ts
+++ packages/rrweb-snapshot/src/utils.ts
@@ -267,14 +267,14 @@ export function isElementSrcBlocked(tagName: string): boolean {
 const EMAIL_REGEX = new RegExp(
   "[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*",
 );
-const LONG_NUMBER_REGEX = new RegExp('d{9,16}'); // unformatted ssn, phone numbers, or credit card numbers
-const SSN_REGEX = new RegExp('d{3}-?d{2}-?d{4}');
+const LONG_NUMBER_REGEX = new RegExp('[0-9]{9,16}'); // unformatted ssn, phone numbers, or credit card numbers
+const SSN_REGEX = new RegExp('[0-9]{3}-?[0-9]{2}-?[0-9]{4}');
 const PHONE_NUMBER_REGEX = new RegExp(
   '[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}',
 );
-const CREDIT_CARD_REGEX = new RegExp('d{4}-?d{4}-?d{4}-?d{4}');
+const CREDIT_CARD_REGEX = new RegExp('[0-9]{4}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}');
 const ADDRESS_REGEX = new RegExp(
-  'd{1,5}.?d{0,3}s[a-zA-Z]{2,30}s[a-zA-Z]{2,15}',
+  '[0-9]{1,5}.?[0-9]{0,3}s[a-zA-Z]{2,30}s[a-zA-Z]{2,15}',
 );
 const IP_REGEX = new RegExp('(?:[0-9]{1,3}.){3}[0-9]{1,3}');
 
