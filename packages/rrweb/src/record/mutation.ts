6cf9dd15dd1d0b3c653e6fd17d5b037679531267
Vadim Korolik <vadim@highlight.io>
2024-02-14 10:45:45 -0800

    fix text obfuscation logic (#116)

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -463,7 +463,7 @@ export default class MutationBuffer {
             text.node?.parentElement?.getAttribute('data-hl-record');
           if (
             (enableStrictPrivacy || obfuscateDefaultPrivacy) &&
-            highlightOverwriteRecord &&
+            !highlightOverwriteRecord &&
             value
           ) {
               value = obfuscateText(value);
