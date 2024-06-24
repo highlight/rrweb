aa511e39a9acb9339fc74bfb7bfed7d223310bf3
Vadim Korolik <vkorolik@gmail.com>
2024-06-24 16:10:41 -0700

    Support default privacy mode (#111)
    Add a privacy mode that will obfuscate personal identifiable data by
    default. Update the strict privacy policy to be part of the enum, rather
    than a boolean.
    
    See https://github.com/highlight/highlight/pull/6846 for testing
    example.
    
    ---------
    
    Co-authored-by: SpennyNDaJets <SpennyNDaJets@users.noreply.github.com>

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -7,6 +7,7 @@ import {
   needMaskingText,
   maskInputValue,
   obfuscateText,
+  shouldObfuscateTextByDefault,
   Mirror,
   isNativeShadowDom,
   getInputType,
@@ -184,7 +185,7 @@ export default class MutationBuffer {
   private keepIframeSrcFn: observerParam['keepIframeSrcFn'];
   private recordCanvas: observerParam['recordCanvas'];
   private inlineImages: observerParam['inlineImages'];
-  private enableStrictPrivacy: observerParam['enableStrictPrivacy'];
+  private privacySetting: observerParam['privacySetting'];
   private slimDOMOptions: observerParam['slimDOMOptions'];
   private dataURLOptions: observerParam['dataURLOptions'];
   private doc: observerParam['doc'];
@@ -211,7 +212,7 @@ export default class MutationBuffer {
         'keepIframeSrcFn',
         'recordCanvas',
         'inlineImages',
-        'enableStrictPrivacy',
+        'privacySetting',
         'slimDOMOptions',
         'dataURLOptions',
         'doc',
@@ -332,7 +333,7 @@ export default class MutationBuffer {
         dataURLOptions: this.dataURLOptions,
         recordCanvas: this.recordCanvas,
         inlineImages: this.inlineImages,
-        enableStrictPrivacy: this.enableStrictPrivacy,
+        privacySetting: this.privacySetting,
         onSerialize: (currentN) => {
           if (isSerializedIframe(currentN, this.mirror)) {
             this.iframeManager.addIframe(currentN as HTMLIFrameElement);
@@ -463,7 +464,11 @@ export default class MutationBuffer {
           }
           /* Begin Highlight Code */
           let value = text.value;
-          if (this.enableStrictPrivacy && value) {
+          const enableStrictPrivacy = this.privacySetting === 'strict';
+          const obfuscateDefaultPrivacy =
+            this.privacySetting === 'default' &&
+            shouldObfuscateTextByDefault(value);
+          if ((enableStrictPrivacy || obfuscateDefaultPrivacy) && value) {
               value = obfuscateText(value);
           }
           return {
@@ -592,6 +597,9 @@ export default class MutationBuffer {
             tagName: target.tagName,
             type,
             value,
+            inputId: target.id,
+            inputName: target.getAttribute('name'),
+            autocomplete: target.getAttribute('autocomplete'),
             maskInputFn: this.maskInputFn,
           });
         }
